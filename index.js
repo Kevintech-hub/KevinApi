92c023004ab22ca8af6c9a6e8a1110e042f0431b
Kevintech-hub <techkevin93@gmail.com>
2026-05-23 07:50:41 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,64 +1,65 @@
 // AI Chat Modules
-export { DeepSeekThinking } from "./src/deepseek.js";
-export { FeelBetter } from "./src/feeb.js";
-export { chat as geminiChat } from "./src/gemini.js";
-export { chat as geminiVisionChat } from "./src/geminiVision.js";
-export { GPT5 } from "./src/gpt5.js";
-export { chat as GPT52Chat } from "./src/gpt52.js";
-export { Qwen3 } from "./src/qwen3.js";
-export { UnlimitedAI, CHARACTERS } from "./src/unlimitedai.js";
+const { DeepSeekThinking } = require("./src/deepseek.js");
+const { FeelBetter } = require("./src/feeb.js");
+const { chat: geminiChat } = require("./src/gemini.js");
+const { chat: geminiVisionChat } = require("./src/geminiVision.js");
+const { GPT5 } = require("./src/gpt5.js");
+const { chat: GPT52Chat } = require("./src/gpt52.js");
+const { Qwen3 } = require("./src/qwen3.js");
+const { UnlimitedAI, CHARACTERS } = require("./src/unlimitedai.js");
 
 // Downloader Modules
-export { aiodl, detectPlatform } from "./src/aio.js";
-export { default as dramaboxsearch } from "./src/dramabox.js";
-export { GoogleSearch } from "./src/google.js";
-export { default as instagramDownloader } from "./src/ig.js";
-export { default as likee } from "./src/likee.js";
-export { default as mediafire } from "./src/mediafire.js";
-export { default as scrapePinterest } from "./src/pindl.js";
-export { RedditDL } from "./src/reddit.js";
-export { RedNoteDL } from "./src/rednote.js";
-export { reelsvideo } from "./src/reelsvideo.js";
-export { default as sfileDownloader } from "./src/sfiledl.js";
-export { default as searchSoundCloud } from "./src/soundcloud.js";
-export { default as soundcloudDownloader } from "./src/soundclouddl.js";
-export { TeraBoxDL } from "./src/terabox.js";
-export { default as tiktokDownloader } from "./src/tiktok.js";
-export { tiktokSearchVideo } from "./src/tiktoksearch.js";
-export { default as twitterDownloader } from "./src/twitter.js";
-export { default as youtubeDownloader } from "./src/youtube.js";
-export { default as ytDownloader } from "./src/yt.js";
-export { ytdl, Youtube, fallbackToMp3Buffer } from "./src/ytdl.js";
+const { aiodl, detectPlatform } = require("./src/aio.js");
+const dramaboxsearch = require("./src/dramabox.js");
+const { GoogleSearch } = require("./src/google.js");
+const instagramDownloader = require("./src/ig.js");
+const likee = require("./src/likee.js");
+const mediafire = require("./src/mediafire.js");
+const scrapePinterest = require("./src/pindl.js");
+const { RedditDL } = require("./src/reddit.js");
+const { RedNoteDL } = require("./src/rednote.js");
+const { reelsvideo } = require("./src/reelsvideo.js");
+const sfileDownloader = require("./src/sfiledl.js");
+const searchSoundCloud = require("./src/soundcloud.js");
+const soundcloudDownloader = require("./src/soundclouddl.js");
+const { TeraBoxDL } = require("./src/terabox.js");
+const tiktokDownloader = require("./src/tiktok.js");
+const { tiktokSearchVideo } = require("./src/tiktoksearch.js");
+const twitterDownloader = require("./src/twitter.js");
+const youtubeDownloader = require("./src/youtube.js");
+const ytDownloader = require("./src/yt.js");
+const { ytdl, Youtube, fallbackToMp3Buffer } = require("./src/ytdl.js");
 
 // Image Processing
-export { upload as hdUpload, get as hdGetStatus } from "./src/hd.js";
-export { default as videoEnhancer, createEnhanceTask, pollEnhanceTask } from "./src/hdvid.js";
-export { Img2Img } from "./src/img2img.js";
-export { default as imgToPrompt } from "./src/img2prompt.js";
-export { default as uploadImage } from "./src/imgdrop.js";
-export { default as ImgUpscaler } from "./src/imglarger.js";
-export { pixa as removeBackground } from "./src/removebackground.js";
-export { default as unrestrictedAiImage } from "./src/txt2img.js";
-export { Txt2Img2 } from "./src/txt2img2.js";
-export { default as upscaler } from "./src/upscaler.js";
-export { default as videyUpload } from "./src/videy.js";
+const { upload: hdUpload, get: hdGetStatus } = require("./src/hd.js");
+const videoEnhancer = require("./src/hdvid.js");
+const { createEnhanceTask, pollEnhanceTask } = require("./src/hdvid.js");
+const { Img2Img } = require("./src/img2img.js");
+const imgToPrompt = require("./src/img2prompt.js");
+const uploadImage = require("./src/imgdrop.js");
+const ImgUpscaler = require("./src/imglarger.js");
+const { pixa: removeBackground } = require("./src/removebackground.js");
+const unrestrictedAiImage = require("./src/txt2img.js");
+const { Txt2Img2 } = require("./src/txt2img2.js");
+const upscaler = require("./src/upscaler.js");
+const videyUpload = require("./src/videy.js");
 
 // Information & Scrapers
-export { default as scrapeHokCharacter } from "./src/hokinfo.js";
-export { default as scrapeWutheringWavesCharacter } from "./src/wwchar.js";
-export { default as wallpaperSearch } from "./src/wallpapersearch.js";
+const scrapeHokCharacter = require("./src/hokinfo.js");
+const scrapeWutheringWavesCharacter = require("./src/wwchar.js");
+const wallpaperSearch = require("./src/wallpapersearch.js");
 
 // Other Utilities
-export { TempMailCreate, TempMailInbox } from "./src/tempmail.js";
-export { default as generateCustomTTS } from "./src/topmedia.js";
+const { TempMailCreate, TempMailInbox } = require("./src/tempmail.js");
+const generateCustomTTS = require("./src/topmedia.js");
 
 // Legacy/Additional
-export { default as spotifyDownloader } from "./src/spotify.js";
+const spotifyDownloader = require("./src/spotify.js");
 
 // Version info
-export const VERSION = "1.0.0";
-export const NAME = "api-kev";
-export const AUTHOR = "Kev";
+const VERSION = "1.0.0";
+const NAME = "API-KEV";
+const AUTHOR = "Kev";
 
 // Default export with all modules
 const API_KEV = {
@@ -102,6 +103,8 @@ const API_KEV = {
   hdUpload,
   hdGetStatus,
   videoEnhancer,
+  createEnhanceTask,
+  pollEnhanceTask,
   Img2Img,
   imgToPrompt,
   uploadImage,
@@ -128,4 +131,62 @@ const API_KEV = {
   AUTHOR
 };
 
-export default API_KEV;
+module.exports = API_KEV;
+
+// Named exports for backward compatibility
+module.exports.DeepSeekThinking = DeepSeekThinking;
+module.exports.FeelBetter = FeelBetter;
+module.exports.geminiChat = geminiChat;
+module.exports.geminiVisionChat = geminiVisionChat;
+module.exports.GPT5 = GPT5;
+module.exports.GPT52Chat = GPT52Chat;
+module.exports.Qwen3 = Qwen3;
+module.exports.UnlimitedAI = UnlimitedAI;
+module.exports.CHARACTERS = CHARACTERS;
+module.exports.aiodl = aiodl;
+module.exports.detectPlatform = detectPlatform;
+module.exports.dramaboxsearch = dramaboxsearch;
+module.exports.GoogleSearch = GoogleSearch;
+module.exports.instagramDownloader = instagramDownloader;
+module.exports.likee = likee;
+module.exports.mediafire = mediafire;
+module.exports.scrapePinterest = scrapePinterest;
+module.exports.RedditDL = RedditDL;
+module.exports.RedNoteDL = RedNoteDL;
+module.exports.reelsvideo = reelsvideo;
+module.exports.sfileDownloader = sfileDownloader;
+module.exports.searchSoundCloud = searchSoundCloud;
+module.exports.soundcloudDownloader = soundcloudDownloader;
+module.exports.TeraBoxDL = TeraBoxDL;
+module.exports.tiktokDownloader = tiktokDownloader;
+module.exports.tiktokSearchVideo = tiktokSearchVideo;
+module.exports.twitterDownloader = twitterDownloader;
+module.exports.youtubeDownloader = youtubeDownloader;
+module.exports.ytDownloader = ytDownloader;
+module.exports.ytdl = ytdl;
+module.exports.Youtube = Youtube;
+module.exports.fallbackToMp3Buffer = fallbackToMp3Buffer;
+module.exports.hdUpload = hdUpload;
+module.exports.hdGetStatus = hdGetStatus;
+module.exports.videoEnhancer = videoEnhancer;
+module.exports.createEnhanceTask = createEnhanceTask;
+module.exports.pollEnhanceTask = pollEnhanceTask;
+module.exports.Img2Img = Img2Img;
+module.exports.imgToPrompt = imgToPrompt;
+module.exports.uploadImage = uploadImage;
+module.exports.ImgUpscaler = ImgUpscaler;
+module.exports.removeBackground = removeBackground;
+module.exports.unrestrictedAiImage = unrestrictedAiImage;
+module.exports.Txt2Img2 = Txt2Img2;
+module.exports.upscaler = upscaler;
+module.exports.videyUpload = videyUpload;
+module.exports.scrapeHokCharacter = scrapeHokCharacter;
+module.exports.scrapeWutheringWavesCharacter = scrapeWutheringWavesCharacter;
+module.exports.wallpaperSearch = wallpaperSearch;
+module.exports.TempMailCreate = TempMailCreate;
+module.exports.TempMailInbox = TempMailInbox;
+module.exports.generateCustomTTS = generateCustomTTS;
+module.exports.spotifyDownloader = spotifyDownloader;
+module.exports.VERSION = VERSION;
+module.exports.NAME = NAME;
+module.exports.AUTHOR = AUTHOR;
